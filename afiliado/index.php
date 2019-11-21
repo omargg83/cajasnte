bb8823175ebaed3e79f9854a3c83c9a536e3f4f2
omargg83 <omargg83@gmail.com>
2019-11-21 11:18:00 -0600

    update
    actualizar

--- afiliado/index.php
+++ afiliado/index.php
@@ -67,6 +67,8 @@
 	</div>*/
 ?>
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/60nwzTKIvqk"
+<iframe width="360" height="315" src="https://www.youtube.com/embed/60nwzTKIvqk"
 frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
 </iframe>
+
+<iframe width="360" height="315" src="https://www.youtube.com/embed/FFrag8ll85w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
