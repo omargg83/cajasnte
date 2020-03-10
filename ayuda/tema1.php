340a334be40b0f5895d585becdd5e9e6cc29ca72
Omar <51249360+omargg83@users.noreply.github.com>
2020-03-09 22:24:18 -0600

    cambios
    cambios

---
+++ ayuda/tema1.php
@@ -0,0 +1,11 @@
+<?php
+echo "<div class='card'>";
+
+
+
+echo "<img src='https://lh3.googleusercontent.com/proxy/q_2eRdtg3atPzD-E2kNrCaPL2UTwXhoJH2qliwsSqsRKcd4ugpA_WLnGB_kYtCL_Y1zFeiFyKbRKqx1vgA6IhFJWuoMtWc4ErdFkMmO67yYm9fsV3rWx8Z0DwzhZEzCSms5GqrKGdDg'>";
+
+
+ ?>
+<button type="button" class="btn btn-outline-secondary btn-sm" data-dismiss="modal" title='Cancelar'><i class="fas fa-sign-out-alt"></i>Cancelar</button>
+</div>
