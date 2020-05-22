1bac29e3fd55829c12c5ab71499a662a31cbdddd
ericesponda <51395158+ericesponda@users.noreply.github.com>
2020-05-21 20:51:54 -0500

    x
    x

--- citas/info.php
+++ citas/info.php
@@ -13,7 +13,7 @@
   <div class="modal-content">
      <div class="modal-header">
        <h5 class="modal-title">Cita</h5>
-       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+       <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
          <span aria-hidden="true">&times;</span>
        </button>
      </div>
@@ -54,6 +54,6 @@
         </div>
      </div>
      <div class="modal-footer">
-       <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+       <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
      </div>
    </div>
