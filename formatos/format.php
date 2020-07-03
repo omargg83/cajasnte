e3e7c99362cff82fbddaa162c58a3615097411c4
Omar <51249360+omargg83@users.noreply.github.com>
2020-07-03 10:15:24 -0500

    Update format.php

--- formatos/format.php
+++ formatos/format.php
@@ -5,8 +5,10 @@
   $fecha_actual = strtotime(date("Y-m-d H:i:s",time()));
   $fecha_entrada = strtotime(fecha($row['fretiro']));
   if($fecha_actual <= $fecha_entrada){
-    echo "<a href='#' class='btn btn-sm' id='imprime_formato' title='Imprimir Formato de retiro con cheque' data-lugar='ahorro/formato' data-tipo='1' title='Formato de retiro'><i class='fas fa-print'></i><span>F. retiro con Cheq.</span></a>";
-    echo "<a href='#' class='btn btn-sm' id='imprime_formato2' title='Imprimir Formato de retiro con Transferencia' data-lugar='ahorro/formato2' data-tipo='1' title='Formato de retiro'><i class='fas fa-print'></i><span>F. retiro con Transf.</span></a>";
+		echo "<div class='jumbotron text-center'>";
+    echo "<a href='#' class='btn btn-warning btn-sm' id='imprime_formato' title='Imprimir Formato de retiro con cheque' data-lugar='formatos/formato' data-tipo='1' title='Formato de retiro'><i class='fas fa-print'></i><span>F. retiro con Cheq.</span></a>";
+    echo "<a href='#' class='btn btn-warning btn-sm' id='imprime_formato2' title='Imprimir Formato de retiro con Transferencia' data-lugar='formatos/formato2' data-tipo='1' title='Formato de retiro'><i class='fas fa-print'></i><span>F. retiro con Transf.</span></a>";
+		echo "</div>";
   }
 
 
